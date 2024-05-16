c21804239a17c3e6bc3bcdef3181540b79a2446b
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2024-05-15 20:33:55 -0400

    it kinda got blocked

--- skibidi/uv-sw.js
+++ skibidi/uv-sw.js
@@ -1,4 +1,4 @@
-importScripts("/static/uv/uv.sw.js");
+importScripts("/skibidi/uv/uv.sw.js");
 
 const sw = new UVServiceWorker();
 
---
+++ skibidi/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/skibidi/tiw/',
+    bare:'https://bare.benrogo.net',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/skibidi/uv/uv.handler.js',
+    bundle: '/skibidi/uv/uv.bundle.js',
+    config: '/skibidi/uv/uv.config.js',
+    sw: '/skibidi/uv/uv.sw.js',
+};
